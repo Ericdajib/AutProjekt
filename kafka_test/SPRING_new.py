4213ce3635645ef941fe826a156e2500db99942e
Ericdajib <xc1044220200@gmail.com>
2022-02-19 21:50:50 +0100

    使用numba加速

--- kafka_test/SPRING_new.py
+++ kafka_test/SPRING_new.py
@@ -2,14 +2,14 @@ import numpy as np
 import matplotlib.pyplot as plt
 import numba as nb
 
-# Q = np.loadtxt("Pulse_current.csv")
-# S_full = np.loadtxt("TEST_current.csv")
-Q = np.array([1,2,3,2,1,2,3,4,5,3,2,1])
-S_full = np.array([1,2,3,2,1,2,3,4,5,3,2,1,7,7,12,15,4,3,8,14,5,1,2,3,2,1,2,3,4,5,6,3,2,1,1,0,4,2,3,6,7,8,2,2,3,2,3,2,3,4,1,2,3,2,1,2,2,3,4,5,6,3,2,1,0,8,10,3,4,5])
+Q = np.loadtxt("Pulse_current.csv")
+S_full = np.loadtxt("TEST_current.csv")
+# Q = np.array([1,2,3,2,1,2,3,4,5,3,2,1])
+# S_full = np.array([1,2,3,2,1,2,3,4,5,3,2,1,7,7,12,15,4,3,8,14,5,1,2,3,2,1,2,3,4,5,6,3,2,1,1,0,4,2,3,6,7,8,2,2,3,2,3,2,3,4,1,2,3,2,1,2,2,3,4,5,6,3,2,1,0,8,10,3,4,5])
 threshold = 36500
 m = len(Q)
 n = 2 * m
-S = np.zeros(m+1)
+S = np.zeros(m)
 S_matched = np.array([])
 D = np.zeros([m,n])
 I = np.zeros([m,n])
@@ -22,7 +22,7 @@ def dist_func(x, y):
 
 
 def plot():
-    global D,I,path,m
+    global D,I,m
     plt.ion()
     plt.clf()
 
@@ -43,13 +43,16 @@ def plot():
     plt.ioff()
 
 
-N = 0
-while True:
-    st = S_full[N]
-    S[1:] = S[:-1]
-    S[0] = st
+@nb.jit(nopython=True)
+def update_stwm(D_unupdated,I_unupdated,sampling,current_value):
+
+    D = D_unupdated
+    I = I_unupdated
     D[:, 1:] = D[:, :-1]
     I[:, 1:] = I[:, :-1]
+    m = len(D)
+    N = sampling
+    st = current_value
 
     if N == 0:
         D[0, 0] = dist_func(Q[0], st)
@@ -71,56 +74,76 @@ while True:
             elif Min == D[i - 1, 0]:
                 I[i, 0] = I[i - 1, 0]
 
-    # track the path
-    if D[m-1,0] < threshold:
-        I_range = int(N-I[m-1, 0])
-        S_matched = S[0:I_range:-1]
-        path = []
-        count = 0
-        i = m - 1
-        j = 0
-
-        while True:
-            if i > 0 and j < I_range:
-                path.append((i,j))
-                Min = min(D[i-1, j+1], D[i,j+1], D[i-1,j])
-                if Min == D[i-1, j+1]:
-                    i = i-1
-                    j = j+1
-                    count = count+1
-                elif Min == D[i, j+1]:
-                    j = j+1
-                    count = count+1
-                elif Min == D[i-1,j]:
-                    i = i-1
-                    count = count+1
-
-            elif i == 0 and j == I_range:
-                path.append((i,j))
-                count = count + 1
-                break
-            elif i == 0:
-                path.append((i,j))
-                j = j + 1  # go to the left
-                count = count + 1
-            elif j == I_range:  # If it goes to the far left
-                path.append((i,j))
-                i = i - 1  # go to the bottom
-                count = count + 1
-
-        print(path, count)
-        print(S_matched[:-1])
+    return D, I
 
 
-    print(f"Abtast: {N+1}")
-    print(f"Wert: {st}" )
-    # print(D,I)
+@nb.jit(nopython=True)
+def track_path(D_updated,I_updated,time_stream,sampling):
+    D = D_updated
+    I = I_updated
+    m = len(D)
+    N = sampling
+    I_range = int(N - I[m - 1, 0])
+    S = time_stream
+    S_matched = S[0:I_range:-1]
+    path = []
+    # count = 0
+    i = m - 1
+    j = 0
+
+    while True:
+        if i > 0 and j < I_range:
+            path.append((i, j))
+            Min = min(D[i - 1, j + 1], D[i, j + 1], D[i - 1, j])
+            if Min == D[i - 1, j + 1]:
+                i = i - 1
+                j = j + 1
+                # count = count + 1
+            elif Min == D[i, j + 1]:
+                j = j + 1
+                # count = count + 1
+            elif Min == D[i - 1, j]:
+                i = i - 1
+                # count = count + 1
+
+        elif i == 0 and j == I_range:
+            path.append((i, j))
+            # count = count + 1
+            break
+        elif i == 0:
+            path.append((i, j))
+            j = j + 1  # go to the left
+            # count = count + 1
+        elif j == I_range:  # If it goes to the far left
+            path.append((i, j))
+            i = i - 1  # go to the bottom
+            # count = count + 1
+
+    return path, S_matched[:-1]
+
+
+N = 0
+while True:
+    st = S_full[N]
+    S[1:] = S[:-1]
+    S[0] = st
+
+    D,I = update_stwm(D,I,N,st)
+
+
+    if D[m - 1, 0] < threshold:
+        path , S_matched = track_path(D,I,S,N)
+    print(D,I)
+    print(f"Abtast: {N + 1}")
+    print(f"Wert: {st}")
 
     plot()
-    if N >= len(S_full)-1:
-        break
 
-    N = N+1
+    if N >= len(S_full) - 1:
+        break
+    N = N + 1
+
+
 
 
 
